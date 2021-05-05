2b308dcc62ef7aef1f11b5e35d2cc7e87f49db97
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-05-05 19:34:08 +0800

    vayu: Disable kpti
    It can also help increase performance by a lot in some scenarios.
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Change-Id: Ica465f586a44129047c266597316045fab762550

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,6 +87,7 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048 msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_BOOTIMG_HEADER_VERSION := 2

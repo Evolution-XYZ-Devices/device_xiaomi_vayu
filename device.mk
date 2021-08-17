d55654d6b5fb5d897463d9b3eaa071d3c802fee6
kubersharma001 <kubersharma001@gmail.com>
2021-08-17 17:37:41 +0000

    vayu: Build libldacBT_bco.so
    08-17 12:10:55.180 19022 19075 E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    
    Change-Id: I910382a3affe8d02d6b0b20fd2dc3e91e57f3e9a

--- device.mk
+++ device.mk
@@ -85,6 +85,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     audio.bluetooth.default \
     libldacBT_dec \
+    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor

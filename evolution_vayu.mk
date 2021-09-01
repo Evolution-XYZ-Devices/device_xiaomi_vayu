a6565c0d925a927a16b9ab17c4e8c52cb12d8345
elizabethangelalorenza <elizabethangelalorenzha@gmail.com>
2021-09-01 19:17:35 +0000

    vayu: Now is Evolution time
    Signed-off-by: elizabethangelalorenza <elizabethangelalorenzha@gmail.com>
    Change-Id: Id57819bc115e8cb4e86a122daf8b8e825d1d3386

---
+++ evolution_vayu.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2021 Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device stuff.
+$(call inherit-product, device/xiaomi/vayu/device.mk)
+
+# Inherit common Evolution-X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := evolution_vayu
+PRODUCT_DEVICE := vayu
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := Poco X3 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="vayu"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

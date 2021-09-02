db984ab486fc9cf8a64825819a12db0bb7ffdc7a
elizabethangelalorenza <elizabethangelalorenzha@gmail.com>
2021-09-02 04:11:40 +0000

    vayu: Compile with Proton Clang
    Signed-off-by: elizabethangelalorenza <elizabethangelalorenzha@gmail.com>
    Change-Id: I925be55208812815da6d826905df59081702712b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,7 @@ TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_KERNEL_CONFIG := vayu_user_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 

10dafa3347c194b0e74631260ac7303e7f1557c3
elizabethangelalorenza <elizabethangelalorenzha@gmail.com>
2021-11-28 13:01:04 +0000

    vayu: Switch to AOSP Clang
    * Switch to AOSP Clang 14.0.1 (based on r437112b)
    
    Change-Id: Ic9994d2ddb2365c77c28d991e07afc7cc314dad9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,7 +90,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := vayu_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_VERSION := r437112b
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument" AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld
 

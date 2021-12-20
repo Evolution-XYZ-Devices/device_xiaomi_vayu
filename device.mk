eb1e03ed0b1877dd8fde77227ed199ec0bcd842f
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-20 09:53:01 +0700

    vayu: perf: Extract perfboostsconfig from vendor tree
    Change-Id: Id3f86f6c36f9b59a05030c4cdb4c0c59f9d08ef0

--- device.mk
+++ device.mk
@@ -423,6 +423,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power.stats@1.0-service.mock \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml
+
 # Properties
 include $(LOCAL_PATH)/properties/default.mk
 
